575232de7afdaa23cc6cbf19ae15540708a0be30
Gokberk Karaca <gskaraca1905@gmail.com>
2018-05-15 20:14:42 +0300

    Add experiment details to result.txt

--- test.sh
+++ test.sh
@@ -2,47 +2,47 @@ make
 javac number_generator.java
 echo
 
-echo "test1";
+echo "test1, 1000 200";
 java NumberGenerator 1000 200 > test1.txt
 ./ds 1000 test1.txt
 
 echo
-echo "test2";
+echo "test2, 1000 400";
 java NumberGenerator 1000 400 > test2.txt
 ./ds 1000 test2.txt
 
 echo
-echo "test3";
+echo "test3, 1000 600";
 java NumberGenerator 1000 600 > test3.txt
 ./ds 1000 test3.txt
 
 echo
-echo "test4";
+echo "test4, 2000 200";
 java NumberGenerator 2000 200 > test4.txt
 ./ds 1000 test4.txt
 
 echo
-echo "test5";
+echo "test5, 2000 400";
 java NumberGenerator 2000 400 > test5.txt
 ./ds 1000 test5.txt
 
 echo
-echo "test6";
+echo "test6, 2000 600";
 java NumberGenerator 2000 600 > test6.txt
 ./ds 1000 test6.txt
 
 echo
-echo "test7";
+echo "test7, 3000 200";
 java NumberGenerator 3000 200 > test7.txt
 ./ds 1000 test7.txt
 
 echo
-echo "test8";
+echo "test8, 3000 400";
 java NumberGenerator 3000 400 > test8.txt
 ./ds 1000 test8.txt
 
 echo
-echo "test9";
+echo "test9, 3000 600";
 java NumberGenerator 3000 600 > test9.txt
 ./ds 1000 test9.txt
 echo
