a9a251b32333b14945c465133736222647d0fcf7
Gokberk Karaca <gskaraca1905@gmail.com>
2018-05-15 20:09:33 +0300

    Decrease experiment number to 9

--- number_generator.java
+++ number_generator.java
@@ -3,13 +3,13 @@ import java.util.*;
 class NumberGenerator {
   public static void main(String[] args) {
 
-    int N = Integer.parseInt(args[0]);
-    int numOfRequests = Integer.parseInt(args[1]);
+    int numOfRequests = Integer.parseInt(args[0]);
+    int N = Integer.parseInt(args[1]);
 
     for (int i = 0; i < numOfRequests; i++) {
       Random random = new Random();
       int number = random.nextInt(N) + 1;
-      System.out.println(i*10 + " " + number);
+      System.out.println(i + " " + number);
     }
   }
 }
